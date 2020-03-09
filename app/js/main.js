dd23f0730905e4aeb0ee07d7397871c95403d3e5
Mher-777 <mherjan2003@mail.ru>
2020-03-09 23:23:27 +0400

    added main-screen

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,36 @@
 $(function () {
-    
+    $('.main-screen__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        speed: 400,
+        rows: 0,
+        cssEase: 'linear',
+        prevArrow: '<button class="main-screen__arrow arrow-left"></button>',
+        nextArrow: '<button class="main-screen__arrow arrow-right"></button>',
+    });
+
+
+
+    let WindowHeight = jQuery(window).height();
+    let load_element = 0;
+    let scroll_position = jQuery('.header').offset().top;
+    let screen_height = jQuery(window).height();
+    let activation_offset = 0;
+    let max_scroll_height = jQuery('body').height() + screen_height;
+    let scroll_activation_point = scroll_position - (screen_height * activation_offset);
+
+    jQuery(window).on('scroll', function (e) {
+
+        let y_scroll_pos = window.pageYOffset;
+        let element_in_view = y_scroll_pos > scroll_activation_point;
+        let has_reached_bottom_of_page = max_scroll_height <= y_scroll_pos && !element_in_view;
+
+        if (element_in_view || has_reached_bottom_of_page) {
+            jQuery('.header').addClass("--sticky");
+        } else {
+            jQuery('.header').removeClass("--sticky");
+        }
+
+    });
 })
