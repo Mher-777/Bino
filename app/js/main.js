73a8db795f6090f330d794640ef6bf7c4ea612d4
Mher-777 <mherjan2003@mail.ru>
2020-03-10 21:57:32 +0400

    added section statistics

--- app/js/main.js
+++ app/js/main.js
@@ -56,5 +56,34 @@ $(function () {
             jQuery('.header').removeClass("--sticky");
         }
     });
+
+    let a = 0;
+    $(window).scroll(function () {
+        var oTop = $('.statistics').offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+            $('.statistics__col-number').each(function () {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                },
+                    {
+                        duration: 3000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            $this.text(this.countNum);
+                        }
+
+                    });
+            });
+            a = 1;
+        }
+
+    });
     var mixer = mixitup('.works');
 })
